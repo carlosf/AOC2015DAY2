df07d50fe7dd5a68effb0a1ee5f7411bc13981d9
Carlos Ferreira <carlosf@northrock.bm>
2023-12-26 23:33:45 -0400

    Initial

---
+++ AOC2015DAY2_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestBox_getSurfaceArea(t *testing.T) {
+	type fields struct {
+		length int
+		width  int
+		height int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		// TODO: Add test cases.
+		{name: ("2x3x4"),
+			fields: fields{2, 3, 4},
+			want:   52},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := Box{
+				length: tt.fields.length,
+				width:  tt.fields.width,
+				height: tt.fields.height,
+			}
+			if got := b.getSurfaceArea(); got != tt.want {
+				t.Errorf("getSurfaceArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
