0cd7ef0d87af77a6c54d40315ffe359119cd638b
Carlos Ferreira <carlosf@northrock.bm>
2023-12-27 23:46:25 -0400

    Completed Day 1 part 1

--- AOC2015DAY2.go
+++ AOC2015DAY2.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
@@ -21,9 +26,42 @@ type Box struct {
 }
 
 func (b Box) getSurfaceArea() int {
-	return 2*b.length*b.width + 2*b.width*b.height + 2*b.height*b.length
+	minside := math.Min(float64(b.length*b.width), float64(b.width*b.height))
+	minside = math.Min(minside, float64(b.height*b.length))
+	fmt.Println("minside is: " + strconv.Itoa(int(minside)))
+	return 2*b.length*b.width + 2*b.width*b.height + 2*b.height*b.length + int(minside)
 }
 
 func main() {
+	p("Start AOC2015DAY2.go")
+	//open input.txt file for
+
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	totalfeet := 0
+
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		p(scanner.Text())
+		boxDim := strings.Split(scanner.Text(), "x")
+		boxlength, _ := strconv.Atoi(boxDim[0])
+		boxwidth, _ := strconv.Atoi(boxDim[1])
+		boxheight, _ := strconv.Atoi(boxDim[2])
+		p("Box length is: " + strconv.Itoa(boxlength))
+		p("Box width is: " + strconv.Itoa(boxwidth))
+		p("Box height is: " + strconv.Itoa(boxheight))
+		myBox := Box{length: boxlength, width: boxwidth, height: boxheight}
+		p("Box surface area is: " + strconv.Itoa(myBox.getSurfaceArea()) + " square feet")
+		totalfeet = totalfeet + myBox.getSurfaceArea()
+
+	}
+	p("Box size is: " + strconv.Itoa(totalfeet))
+	p("End AOC2015DAY2.go")
 
 }
--- AOC2015DAY2_test.go
+++ AOC2015DAY2_test.go
@@ -14,9 +14,9 @@ func TestBox_getSurfaceArea(t *testing.T) {
 		want   int
 	}{
 		// TODO: Add test cases.
-		{name: ("2x3x4"),
+		{name: "2x3x4",
 			fields: fields{2, 3, 4},
-			want:   52},
+			want:   58},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
